7144a701ae6a8ff15e23207807d91505a147c957
TonyDamov <antoan.damov@gmail.com>
2024-06-16 21:27:40 +0300

    Added display of new models in admin panell

--- Stivans/catalogue/admin.py
+++ Stivans/catalogue/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Cart, Item
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'item']
+    # Add any other details you want to display in the list
+
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['rating', 'image', 'name', 'description', 'price']
+    # Add any other details you want to display in the list
