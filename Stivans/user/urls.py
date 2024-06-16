07e61ba5bfde5776297867e0aa7695aa9f6dbeeb
TonyDamov <antoan.damov@gmail.com>
2024-06-16 12:35:38 +0300

    Added logout html file. Error with LogoutView.

--- Stivans/user/urls.py
+++ Stivans/user/urls.py
@@ -21,4 +21,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', user_views.Register, name='register'),
     path('login/', user_views.CustomLoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout')
 ]
--- Stivans/user/views.py
+++ Stivans/user/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.views import LoginView
+
 # Create your views here.
 
 def Register(request):
